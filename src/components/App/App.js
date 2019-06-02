e429ba7cb6aa64ae5cdbe358135b550677905cdf
Kevin Krato <kratok08@gmail.com>
2019-06-02 16:59:36 -0600

    Extract navbar to func component and handle routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,11 +3,13 @@ import LoginPage from "../LoginPage/LoginPage";
 import DashBoard from "../Dashboard/Dashboard";
 import "./App.scss";
 import { Route } from "react-router";
+import { NavBar } from "../NavBar/NavBar";
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Route exact path="/" component={LoginPage} />
+        <Route path="/:user" component={NavBar} />
         <Route exact path="/:user" component={DashBoard} />
       </div>
     );
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,34 +1,10 @@
 import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
 import "./Dashboard.scss";
 
 class DashBoard extends Component {
   render() {
     return (
       <React.Fragment>
-        <nav className="NavBar">
-          <div className="nav-left">
-            <div className="logo">
-              <i className="material-icons">account_balance</i>
-              <h3>dieBörse</h3>
-            </div>
-            <div className="search-bar">
-              <i className="material-icons">search</i>
-              <input type="search" placeholder="Search" />
-            </div>
-          </div>
-          <div className="nav-links">
-            <NavLink activeClassName="active" to="/">
-              <span>Home</span>
-            </NavLink>
-            <NavLink activeClassName="active" to="/notifications">
-              <span>Notifications</span>
-            </NavLink>
-            <NavLink activeClassName="active" to="/account">
-              <span>Account</span>
-            </NavLink>
-          </div>
-        </nav>
         <main>
           <div className="LineGraph">LineGraph</div>
         </main>
