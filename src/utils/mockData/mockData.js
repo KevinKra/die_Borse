9ec75262ab411d41b446db6f182997415b86c291
Kevin Krato <kratok08@gmail.com>
2019-06-02 17:07:13 -0600

    Create mockdata

---
+++ src/utils/mockData/mockData.js
@@ -0,0 +1,51 @@
+export const mockStocksDaily = [
+  {
+    open: 145.23,
+    high: 124.615,
+    low: 123.32,
+    close: 123.68,
+    volume: 19946769
+  },
+  {
+    open: 133.23,
+    high: 144.615,
+    low: 121.32,
+    close: 145.68,
+    volume: 26635769
+  },
+  {
+    open: 133.23,
+    high: 124.615,
+    low: 123.32,
+    close: 13.68,
+    volume: 21146769
+  },
+  {
+    open: 124.23,
+    high: 124.615,
+    low: 123.32,
+    close: 123.68,
+    volume: 26646769
+  },
+  {
+    open: 124.23,
+    high: 134.615,
+    low: 123.32,
+    close: 123.68,
+    volume: 26226769
+  },
+  {
+    open: 124.23,
+    high: 124.615,
+    low: 113.32,
+    close: 123.68,
+    volume: 26632769
+  },
+  {
+    open: 154.23,
+    high: 114.615,
+    low: 123.32,
+    close: 123.68,
+    volume: 26646769
+  }
+];
